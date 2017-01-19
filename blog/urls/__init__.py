3311797b021cd88da2d15e77455b2423bb14dc2b
jiaxin <edison7500@gmail.com>
2017-01-19 10:41:14 +0800

    update

--- blog/urls/__init__.py
+++ blog/urls/__init__.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from blog.views import BlogListView, BlogDetailView
+from blog.views import BlogListView, BlogDetailView, PostTagListView
 
 
 urlpatterns = [
     url(r'^$', BlogListView.as_view(), name='web_blog_list'),
     url(r'^(?P<slug>[\w|\-]+)/?', BlogDetailView.as_view(), name='web_blog_detail'),
+    url(r'^tag/(?P<tag_id>\d+/?)', PostTagListView.as_view(), name='web_blog_tags'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from tagging.models import TaggedItem, Tag
 from blog.models import Post
 
 # Create your views here.
@@ -18,3 +19,20 @@ class BlogDetailView(DetailView):
     template_name           = 'blog/detail.html'
     slug_field              = 'slug'
 
+
+class PostTagListView(ListView):
+    http_method_names       = ['get']
+    model                   = TaggedItem
+    template_name           = 'blog/tag/list.html'
+    paginate_by             = 30
+
+    def get_queryset(self):
+        thing_tag = Tag.objects.get(pk=self.tag_id)
+        # log.info(thing_tag)
+        queryset  = TaggedItem.objects.get_by_model(Post, thing_tag)
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        self.tag_id     = kwargs.pop('tag_id', None)
+        assert self.tag_id is not None
+        return super(PostTagListView, self).get(request, *args, **kwargs)
