481d804bddd0e71cd9eae0c5fa1371804dcfc55e
jiaxin <edison7500@gmail.com>
2017-01-23 12:02:22 +0800

    update

--- blog/admin/post.py
+++ blog/admin/post.py
@@ -9,6 +9,7 @@ class PostAdmin(admin.ModelAdmin):
 
     list_display    = ('title', 'status', 'created_date', 'last_update')
     list_filter     = ('status', )
+    search_fields   = ('title', )
 
 
 
