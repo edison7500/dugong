078028a3bd844b45964845d149fac0670fe47693
jiaxin <edison7500@gmail.com>
2017-02-02 15:07:06 +0800

    update

--- books/templatetags/book_extras.py
+++ books/templatetags/book_extras.py
@@ -12,7 +12,10 @@ from books.models import Book
 @register.inclusion_tag('books/trending.html')
 def trending_books(tags=None):
     if tags is not None:
-        _books = TaggedItem.objects.get_union_by_model(Book.objects.filter(status=True), tags)[:6]
+        _books = TaggedItem.objects.get_union_by_model(Book.objects.filter(status=True), tags)
+        _count = len(_books)
+        if _count > 6:
+            _books  = random.sample(_books, 6)
         return {'books': _books}
     else:
         return {'books': None}
