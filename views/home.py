f5b8c627c2c59f2bd06659d589b1374fead7eeb6
jiaxin <edison7500@gmail.com>
2017-02-24 14:46:16 +0800

    update

--- views/home.py
+++ views/home.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
+from blog.models import Post
 
 
 
 class HomeView(TemplateView):
 
     template_name = "index/home.html"
+
+    def get_context_data(self, **kwargs):
+        _context    = super(HomeView, self).get_context_data(**kwargs)
+        latest_obj  = Post.objects.filter(status=Post.publish)[:10]
+        _context['latest_posts'] = latest_obj
+        return _context
