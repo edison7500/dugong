b48aa68f744bf1185e5da3175f7ab4c0e0133ff9
jiaxin <edison7500@gmail.com>
2017-04-04 16:53:03 +0800

    update

--- views/home.py
+++ views/home.py
@@ -1,8 +1,7 @@
 from django.views.generic import TemplateView
 # from blog.models import Post
-# from opensource.models import Project
+# from opensource.models import Project, Category
 from haystack.query import SearchQuerySet
-
 
 
 class HomeView(TemplateView):
@@ -11,5 +10,6 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         _context    = super(HomeView, self).get_context_data(**kwargs)
-        _context['projects']    = SearchQuerySet().filter(display=True).order_by('-star')[:100]
+
+        _context['projects']    = SearchQuerySet().filter(display=True, category=1).order_by('-star')[:100]
         return _context
