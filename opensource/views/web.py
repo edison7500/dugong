925e0a118ed759f6ea312fa67e0acd39330b025c
jiaxin <edison7500@gmail.com>
2017-04-24 23:58:36 +0800

    update

--- opensource/views/web.py
+++ opensource/views/web.py
@@ -3,8 +3,16 @@
 from django.views.generic import DetailView
 from silk.profiling.profiler import silk_profile
 
-from opensource.models import Project
+from opensource.models import Project, Status
 
+# from django.utils.log import getLogger
+
+# log = getLogger('django')
+
+from datetime import timedelta
+from datetime import datetime
+
+import pygal
 
 
 class ProjectDetailView(DetailView):
@@ -13,16 +21,24 @@ class ProjectDetailView(DetailView):
     model           = Project
     slug_field      = 'identified_code'
     context_object_name = 'project'
-    # query_pk_and_slug =
 
     def get_context_data(self, **kwargs):
         _context    = super(ProjectDetailView, self).get_context_data(**kwargs)
+
+        _status     = Status.objects.filter(project=self.object,
+                                            datetime__gte=datetime.now() - timedelta(30)
+                                            )
+        line_chat           = pygal.Line()
+        line_chat.x_labels  = map(lambda x: x.datetime.strftime("%Y-%m-%d"), _status)
+        line_chat.title     = self.object
+
         _context.update({
             'meta': {
                 'author': self.object.author,
                 # 'desc': "{author} - {desc}".format(author=self.object.author,
                 #                                    desc=self.object.desc.encode('utf8')),
-            }
+            },
+            'chat': line_chat,
         })
         return _context
 
