955a746e440be748e3b6d96d664d9c261041d0d2
jiaxin <edison7500@gmail.com>
2017-06-06 14:30:13 +0800

    update

--- tutorials/models.py
+++ tutorials/models.py
@@ -12,9 +12,11 @@ class Category(models.Model):
 class Tutorial(models.Model):
     title = models.CharField(max_length=128, blank=True)
     slug = models.SlugField(max_length=64, unique=True)
+    category = models.ForeignKey(Category, related_name='tutorial')
     content = models.TextField()
     created_datetime = models.DateTimeField(default=timezone.now, db_index=True, editable=False)
     updated_datetime = models.DateTimeField(auto_now=True, db_index=True, editable=False)
 
-    
+    def __str__(self):
+        return '{}'.format(self.title)
 
