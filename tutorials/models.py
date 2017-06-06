dbefbe2b046c10693a21b73cf4cb97a8b6b10bb6
jiaxin <edison7500@gmail.com>
2017-06-06 14:25:57 +0800

    update

--- tutorials/models.py
+++ tutorials/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=128, blank=True)
+    slug = models.SlugField(max_length=64, unique=True)
+    content = models.TextField()
+    created_datetime = models.DateTimeField(default=timezone.now, db_index=True, editable=False)
+    updated_datetime = models.DateTimeField(auto_now=True, db_index=True, editable=False)
+
+    
+
