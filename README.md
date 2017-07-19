566c2432afb077fc7af85a1d740ec3a6276f00e2
jiaxin <edison7500@gmail.com>
2017-07-19 12:47:44 +0800

    add pandas

--- README.md
+++ README.md
@@ -21,46 +21,6 @@ docker run -d -t -i \
         -p 127.0.0.1:8000:8000 dugong
 ~~~~
 
-### Django 第三方组建
-```
-crcmod==1.7
-qiniu==7.1.0
-Django==1.8.15
-django-appconf==1.0.2
-django-compressor==2.1
-django-debug-toolbar==1.6
-django-qiniu-storage==2.3.0
-django-formtools==1.0
-django-simple-captcha==0.5.3
-#django-wysiwyg-redactor==0.5.0
-django-uuslug==1.1.8
-django-tagging==0.4.5
-django-markdown==0.8.4
-djangorestframework==3.5.3
-django-filter==1.0.1
-django-htmlmin==0.10.0
-django-cache-machine==0.9.1
-gevent==1.1.2
-greenlet==0.4.10
-gunicorn==19.6.0
-Pillow==4.0.0
-requests==2.13.0
-six==1.10.0
-sqlparse==0.2.1
-Unidecode==0.4.19
-MySQL-python==1.2.5
-diskcache==2.3.0
-
-jieba==0.38
-Whoosh==2.7.4
-django-haystack==2.6.0
-
-django-silk==1.0.0
-
-Pygments==2.1.3
-pygments-markdown-lexer==0.1.0.dev39
-
-```
 
 ### 教程
 * [利用 Django 建站攻略 （一）安装](http://jiaxin.im/blog/li-yong-django-jian-zhan-gong/)
--- opensource/views/web.py
+++ opensource/views/web.py
@@ -27,22 +27,15 @@ class ProjectDetailView(DetailView):
         _context    = super(ProjectDetailView, self).get_context_data(**kwargs)
 
         _status     = Status.objects.filter(project=self.object,
-                                            datetime__gte=datetime.now() - timedelta(15)
+                                            datetime__gte=datetime.now() - timedelta(31)
                                             ).order_by('datetime')
-        # line_chat           = pygal.Line(x_label_rotation=90, width=600, height=300, pretty_print=True,
-        #                                  interpolate='cubic', style=DarkSolarizedStyle)
-        # line_chat.human_readable    = True
-        # line_chat.x_labels  = map(lambda x: x.datetime.strftime("%Y-%m-%d"), _status)
-        # line_chat.add('star', map(lambda x: x.star, _status))
-        # # line_chat.add('watch', map(lambda x: x.watch, _status))
-        # # line_chat.add('fork', map(lambda x: x.fork, _status))
-        # line_chat.title     = self.object.name
+        chart = self.get_chart(_status)
 
         _context.update({
             'meta': {
                 'author': self.object.author,
             },
-            'chat': self.get_chart(_status),
+            'chart': chart.render(show_dots=True),
         })
         return _context
 
@@ -53,12 +46,16 @@ class ProjectDetailView(DetailView):
                                width=600, height=300,
                                pretty_print=True,
                                 interpolate='cubic', style=DarkSolarizedStyle)
-
-        line_chat.x_labels = map(lambda x: x.datetime.strftime("%Y-%m-%d"), stats)
+        line_chat.human_readable = True
+        line_chat.x_labels = map(lambda x: x.datetime.strftime("%Y-%m-%d"), stats[1:])
         star_se = df.star.diff().fillna(0)
-        line_chat.add('star', [row for row in star_se])
-
-        return line_chat.render(show_dots=True),
+        fork_se = df.fork.diff().fillna(0)
+        watch_se = df.watch.diff().fillna(0)
+        line_chat.add('star', [row for row in star_se[1:]])
+        line_chat.add('fork', [row for row in fork_se[1:]])
+        line_chat.add('watch', [row for row in watch_se[1:]])
+        line_chat.title = self.object.name
+        return line_chat
 
     @silk_profile(name='Project Detail View')
     def get(self, request, *args, **kwargs):
