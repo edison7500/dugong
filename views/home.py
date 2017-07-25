faae71b1802eded4a29e134b0ea524f93c5f4daf
jiaxin <edison7500@gmail.com>
2017-07-25 16:01:07 +0800

    update

--- views/home.py
+++ views/home.py
@@ -10,10 +10,11 @@ class HomeView(TemplateView):
     template_name = "index/home.html"
 
     def get_context_data(self, **kwargs):
-        _context    = super(HomeView, self).get_context_data(**kwargs)
-
-        _context['projects']    = SearchQuerySet().filter_and(display=True,
-                                                          category='python').order_by('-latest_30_day_star')[:100]
+        _context = super(HomeView, self).get_context_data(**kwargs)
+        _context['projects_week'] = SearchQuerySet().filter_and(display=True,
+                                                          category='python').order_by('-latest_7_day_star')[:20]
+        _context['projects_month'] = SearchQuerySet().filter_and(display=True,
+                                                           category='python').order_by('-latest_30_day_star')[:20]
         return _context
 
     @silk_profile(name='HomePage View')
