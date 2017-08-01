2b135a160a0c418e8c7389cc93447b07d38216a0
jiaxin <edison7500@gmail.com>
2017-08-01 17:04:05 +0800

    updat

--- scripts/post_author.py
+++ scripts/post_author.py
@@ -1,3 +1,18 @@
 import requests
 
 
+headers = {
+    'Authorization': 'Token ad1eb35acec4d816f868e95fef394f1285f3d26c',
+}
+
+s = requests.session()
+
+
+res = s.get(url='http://www.jiaxin.im/api/opensource/', headers=headers)
+
+data = res.json()
+
+results = data.get('results', None)
+
+for row in results:
+    print(row['github_url'])
