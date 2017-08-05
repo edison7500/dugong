a3a2dbc817c2f2b5401066ecea2643bcfcbfbf88
jiaxin <edison7500@gmail.com>
2017-08-05 23:12:36 +0800

    update

--- static/js/dugong.js
+++ static/js/dugong.js
@@ -75,7 +75,7 @@
                 })
             }
         });
-        $("p >img").addClass('img-responsive');
+        // $("p >img").addClass('img-responsive');
     })();
 
 }(jQuery, document, window));
