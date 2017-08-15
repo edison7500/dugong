662ac4f357472009b51b00fb2218386a4d61b1a4
jiaxin <edison7500@gmail.com>
2017-08-15 14:26:39 +0800

    update

--- tutorials/admin/tutorial.py
+++ tutorials/admin/tutorial.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
+from django_markdown.widgets import AdminMarkdownWidget
+from django.db import models
 
 class TutorialAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMarkdownWidget}
+    }
 
 
