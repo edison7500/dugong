607bb04fae2de836fbc17f9d300ac741f805a92b
jiaxin <edison7500@gmail.com>
2017-08-18 00:57:21 +0800

    update

--- tutorials/admin/tutorial.py
+++ tutorials/admin/tutorial.py
@@ -2,11 +2,18 @@
 from django.contrib import admin
 from django_markdown.widgets import AdminMarkdownWidget
 from django.db import models
+from tutorials.models import TutorialImage
+
+class TutorialImageInlineAdmin(admin.StackedInline):
+    model = TutorialImage
+    extra = 1
+
 
 class TutorialAdmin(admin.ModelAdmin):
     list_display = ['title', 'tags', 'status', 'published_at']
     formfield_overrides = {
         models.TextField: {'widget': AdminMarkdownWidget}
     }
+    inlines = [TutorialImageInlineAdmin, ]
 
 
