d4324afc842a5429a7789069d90cf9376513e363
jiaxin <edison7500@gmail.com>
2018-01-25 09:53:59 +0800

    fixed bug in python3

--- opensource/search_indexes.py
+++ opensource/search_indexes.py
@@ -3,17 +3,17 @@ from opensource.models import Project
 
 
 class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
-    text        = indexes.CharField(document=True, use_template=True)
-    author      = indexes.CharField(model_attr='author',)
-    name        = indexes.CharField(model_attr='name',)
-    category    = indexes.CharField(model_attr='category')
-    desc        = indexes.CharField(model_attr='desc')
-    url         = indexes.CharField(model_attr='get_absolute_url')
-    display     = indexes.BooleanField(model_attr='display')
+    text = indexes.CharField(document=True, use_template=True)
+    author = indexes.CharField(model_attr='author', )
+    name = indexes.CharField(model_attr='name', )
+    category = indexes.CharField(model_attr='category')
+    desc = indexes.CharField(model_attr='desc')
+    url = indexes.CharField(model_attr='get_absolute_url')
+    display = indexes.BooleanField(model_attr='display')
 
-    star        = indexes.IntegerField(model_attr='latest_star')
-    watch       = indexes.IntegerField(model_attr='latest_watch')
-    fork        = indexes.IntegerField(model_attr='latest_fork')
+    star = indexes.IntegerField(model_attr='latest_star')
+    watch = indexes.IntegerField(model_attr='latest_watch')
+    fork = indexes.IntegerField(model_attr='latest_fork')
 
     latest_30_day_star = indexes.IntegerField(default=0, stored=True)
     latest_30_day_fork = indexes.IntegerField(default=0, stored=True)
@@ -23,7 +23,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     latest_7_day_fork = indexes.IntegerField(default=0, stored=True)
     latest_7_day_watch = indexes.IntegerField(default=0, stored=True)
 
-    name_auto   = indexes.EdgeNgramField(model_attr='name')
+    name_auto = indexes.EdgeNgramField(model_attr='name')
 
     def get_model(self):
         return Project
@@ -38,6 +38,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     def prepare_latest_30_day_fork(self, obj):
         fork_sum = obj.stats_df().fork.diff().fillna(0).sum()
         return int(fork_sum)
+
     #
     def prepare_latest_30_day_watch(self, obj):
         watch_sum = obj.stats_df().watch.diff().fillna(0).sum()
@@ -50,6 +51,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     def prepare_latest_7_day_fork(self, obj):
         fork_sum = obj.stats_df(8).fork.diff().fillna(0).sum()
         return int(fork_sum)
+
     #
     def prepare_latest_7_day_watch(self, obj):
         watch_sum = obj.stats_df(8).watch.diff().fillna(0).sum()
