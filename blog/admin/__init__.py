1eb0381087c1087535520aa84d43a0fd51fb5aa8
jiaxin <edison7500@gmail.com>
2018-01-30 15:45:28 +0800

    update

--- blog/admin/__init__.py
+++ blog/admin/__init__.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.flatpages.models import FlatPage
+# from django.contrib.flatpages.models import FlatPage
 
-from .post import PostAdmin, PostImageAdmin, FlatPageAdmin
+from .post import PostAdmin, PostImageAdmin
 from blog.models import Post, PostImage
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(PostImage, PostImageAdmin)
 
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
+# admin.site.unregister(FlatPage)
+# admin.site.register(FlatPage, FlatPageAdmin)
