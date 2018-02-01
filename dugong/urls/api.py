4e4bd023ac90088dab6229906e837323afda763b
jiaxin <edison7500@gmail.com>
2018-02-01 09:42:57 +0800

    update urls config

--- dugong/urls/api.py
+++ dugong/urls/api.py
@@ -6,7 +6,6 @@ schema_view = get_schema_view(title='JiaXin API')
 
 urlpatterns = [
     url(r'^opensource/', include('opensource.urls.api', namespace='opensource')),
-
     url(r'^schema/$', schema_view),
 ]
 
