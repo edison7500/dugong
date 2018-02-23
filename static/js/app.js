e2fcdd3a501f62d95615a18b174678451ea3adec
jiaxin <edison7500@gmail.com>
2018-02-23 17:41:13 +0800

    update js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,13 @@
-// require("./dugong");
+require('codemirror/lib/codemirror.css'); // codemirror
+require('tui-editor/dist/tui-editor.css'); // editor ui
+require('tui-editor/dist/tui-editor-contents.css'); // editor content
+require('highlight.js/styles/github.css'); // code block highlight
+
+var Editor = require('tui-editor');
+
+var editor = new Editor({
+    el: document.querySelector('#editSection'),
+    initialEditType: 'markdown',
+    previewStyle: 'vertical',
+    height: '300px'
+});
--- tutorials/forms/tutorial.py
+++ tutorials/forms/tutorial.py
@@ -1,6 +1,6 @@
 from django import forms
 from tutorials.models import Tutorial
-from .widgets import BSMarkDownWidget
+# from .widgets import BSMarkDownWidget
 
 
 class TutorialForm(forms.ModelForm):
@@ -8,13 +8,10 @@ class TutorialForm(forms.ModelForm):
     class Meta:
         model = Tutorial
         fields = ['title', 'content', 'status', 'tags', 'origin_link']
-        # widgets = {
-        #     'content': forms.Textarea(attrs={"data-provide":"markdown",
-        #                                      "data-iconlibrary":"fa",
-        #                                      "id":"editor",
-        #                                      })
-        # }
         widgets = {
-            'content': BSMarkDownWidget()
+            'content': forms.Textarea()
         }
+        # widgets = {
+        #     'content': BSMarkDownWidget()
+        # }
 
