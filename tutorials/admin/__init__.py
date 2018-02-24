9893e587d0c85d685801147de251927851e59e9a
jiaxin <edison7500@gmail.com>
2018-02-24 20:57:46 +0800

    update

--- tutorials/admin/__init__.py
+++ tutorials/admin/__init__.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .tutorial import TutorialAdmin
 from tutorials.models import Tutorial
 
-admin.site.register(Tutorial, TutorialAdmin)
+# admin.site.register(Tutorial, TutorialAdmin)
