abb2d38a7a381a759c05731e43a54f1296727842
jiaxin <edison7500@gmail.com>
2018-05-15 11:54:02 +0800

    update

---
+++ static/webpack.config.production.js
@@ -0,0 +1,16 @@
+const path = require('path');
+// const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'editor.js'
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ]
+};
