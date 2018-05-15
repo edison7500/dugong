091c4e647bf8196632369b34a37dd77ec8503a8c
jiaxin <edison7500@gmail.com>
2018-05-15 15:41:53 +0800

    update

--- dugong/urls/api.py
+++ dugong/urls/api.py
@@ -4,17 +4,7 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Jiaxin API')
 
-# urlpatterns = [
-#     url(r'^$', schema_view)
-# ]
-#
-# from rest_framework.schemas import get_schema_view
-#
-# schema_view = get_schema_view(title='JiaXin API', public=False)
-
 urlpatterns = [
-    # url(r'^auth/', include('rest_auth.urls')),
-    # url(r'^auth/registration/', include('rest_auth.registration.urls')),
     url(r'^opensource/', include('opensource.urls.api', namespace='opensource')),
     url(r'^tutorials/', include('tutorials.urls.api', namespace='tutorials')),
     url(r'^docs/$', schema_view),
