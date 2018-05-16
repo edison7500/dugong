d08ffdfe3ace75fff9fd29dd549dbbe5b768c1d4
jiaxin <edison7500@gmail.com>
2018-05-16 22:42:13 +0800

    update django settings

--- dugong/settings/stage.py
+++ dugong/settings/stage.py
@@ -1,8 +1,21 @@
-from .settings import *
+from .base import *
+from .base import env
 
-INSTALLED_APPS += (
+SECRET_KEY = env('SECRET_KEY', default='rlch5d=b=i@gwuyjntbk#weuu0tne4*)b4hq1hb^h_tq-0=e@u')
+
+ALLOWED_HOSTS = [
+    "*",
+]
+
+# django session
+# -----------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/1.11/topics/http/sessions/
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+SESSION_FILE_PATH = '/tmp/'
+
+INSTALLED_APPS += [
     'django_nose',
-)
+]
 
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
@@ -12,3 +25,5 @@ NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=blog,opensource,tutorials',
 ]
+
+
