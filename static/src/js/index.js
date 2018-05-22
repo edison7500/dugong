66b2b0adb7cc0624388b76e68bf7358f605938e0
jiaxin <edison7500@gmail.com>
2018-05-22 23:09:18 +0800

    update

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -7,15 +7,6 @@ import plugin from './plugin';
 plugin('stickysidebar', StickySidebar);
 
 $(document).ready(function(){
-//     $("p").click(function(){
-//         $(this).hide();
-//     });
-//     var sidebar = new StickySidebar('#sidebar', {
-//         containerSelector: '#main-content',
-//         innerWrapperSelector: '.sidebar__inner',
-//         topSpacing: 20,
-//         bottomSpacing: 20
-//     });
     $("#sidebar").stickysidebar({
         // containerSelector: '.columns',
         // innerWrapperSelector: '.sidebar__inner',
