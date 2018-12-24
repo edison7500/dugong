fd14d364153805a8790115a69293bfb3c2bf8ca1
jiaxin <edison7500@gmail.com>
2018-12-24 17:44:35 +0800

    update

--- apps/images/setup.py
+++ apps/images/setup.py
@@ -8,11 +8,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-images',
+    name='django-generic-images',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
+    license='GPL-3.0 License',
     description='A Django app to conduct Web-based images.',
     long_description=README,
     url='https://jiaxin.im/',
