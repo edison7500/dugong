d3a7d710e77bdccf679926470e55b1c84ab7e8e1
jiaxin <edison7500@gmail.com>
2019-02-27 12:28:10 +0800

    remove tutorial update and crate

--- apps/tutorials/urls/__init__.py
+++ apps/tutorials/urls/__init__.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
+
 from apps.tutorials.views.web import (
     TutorialListView,
-    TutorialDetailView,
-    TutorialCreateView,
-    TutorialUpdateView
+    TutorialDetailView
 )
 
 urlpatterns = [
     url(r'^$', TutorialListView.as_view(), name='list'),
     url(r'^(?P<slug>\d+)\.htm$', TutorialDetailView.as_view(), name='detail'),
-    url(r'^create\.htm$', TutorialCreateView.as_view(), name='create'),
-    url(r'^(?P<slug>\d+)/update\.htm$', TutorialUpdateView.as_view(), name='update'),
+    # url(r'^create\.htm$', TutorialCreateView.as_view(), name='create'),
+    # url(r'^(?P<slug>\d+)/update\.htm$', TutorialUpdateView.as_view(), name='update'),
 ]
