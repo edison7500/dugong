eb40c898a727f69cc2a360e2e32628ace59cd3c0
jiaxin <edison7500@gmail.com>
2019-03-14 18:12:15 +0800

    add user id in api/rest/profile/

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -10,5 +10,5 @@ class UserDetailsSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = UserModel
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
         read_only_fields = ('email', )
