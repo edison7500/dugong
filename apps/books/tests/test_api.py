3fa4282e8c11b6cd164311c3d088eee4402005ed
jiaxin <edison7500@gmail.com>
2019-04-09 15:52:41 +0800

    add test file in book app

---
+++ apps/books/tests/test_api.py
@@ -0,0 +1,13 @@
+from rest_framework.test import APITestCase
+
+
+class BookAPITestCase(APITestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_get_books(self):
+        pass
+
+    def test_create_book(self):
+        pass
