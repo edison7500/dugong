a3344a3c3ef86bedc14fd37e4bd1710c6fa20792
jiaxin <edison7500@gmail.com>
2019-06-04 16:03:25 +0800

    fixed bug

--- apps/blog/views/__init__.py
+++ apps/blog/views/__init__.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView
 
@@ -57,17 +58,19 @@ class BlogDetailView(DetailView):
 
 
 class PostTagListView(ListView):
-    http_method_names = ["get", "head"]
-    model = TaggedItem
+    # http_method_names = ["get", "head"]
+    model = Post
+    queryset = Post.objects.all()
     template_name = "blog/tag/list.html"
     paginate_by = 30
 
     def get_queryset(self):
-        self.thing_tag = Tag.objects.get(pk=self.tag_id)
-        queryset = TaggedItem.objects.get_by_model(
-            Post.objects.filter(status=Post.publish), self.thing_tag
-        )
-        return queryset
+        try:
+            self.thing_tag = Tag.objects.get(pk=self.kwargs.pop("tid"))
+        except Tag.DoesNotExist:
+            raise Http404
+        qs = super().get_queryset().filter(tags__name__in=[self.thing_tag.name])
+        return qs
 
     def get_context_data(self, **kwargs):
         _context_data = super(PostTagListView, self).get_context_data(**kwargs)
@@ -75,6 +78,6 @@ class PostTagListView(ListView):
         return _context_data
 
     def get(self, request, *args, **kwargs):
-        self.tag_id = kwargs.pop("tag_id", None)
-        assert self.tag_id is not None
+        # self.tag_id = kwargs.pop("tag_id", None)
+        # assert self.tag_id is not None
         return super(PostTagListView, self).get(request, *args, **kwargs)
