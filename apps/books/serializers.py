9af110333573cef05fc70886e435a0a6039a54b3
jiaxin <edison7500@gmail.com>
2019-06-13 16:39:41 +0800

    update

--- apps/books/serializers.py
+++ apps/books/serializers.py
@@ -2,13 +2,14 @@ import logging
 
 from rest_framework import serializers
 
+from taggit_serializer.serializers import TaggitSerializer
 from apps.ext.rest.serializers.taggit import TaggitListSerializerField
 from .models import Book
 
 logger = logging.getLogger("django")
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(TaggitSerializer, serializers.ModelSerializer):
     # tags = serializers.ListField(source="tag_list")
     tags = TaggitListSerializerField(required=False)
 
@@ -17,10 +18,10 @@ class BookSerializer(serializers.ModelSerializer):
         exclude = ["id", "identified", "status", "created_at", "updated_at"]
         extra_kwargs = {"origin_link": {"write_only": True}}
 
-    def create(self, validated_data):
-        tag_list = validated_data.pop("tag_list", None)
-        # logger.info(tag_list)
-        instance = super().create(validated_data)
-        for tag in tag_list:
-            instance.tags.add(tag)
-        return instance
+    # def create(self, validated_data):
+    #     tag_list = validated_data.pop("tag_list", None)
+    #     # logger.info(tag_list)
+    #     instance = super().create(validated_data)
+    #     for tag in tag_list:
+    #         instance.tags.add(tag)
+    #     return instance
