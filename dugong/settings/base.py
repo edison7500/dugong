3653363f9c1e45408e17145be61c4821491201bc
jiaxin <edison7500@gmail.com>
2019-07-10 10:16:09 +0800

    update

--- dugong/settings/base.py
+++ dugong/settings/base.py
@@ -80,7 +80,7 @@ LOCAL_APPS = [
     "apps.tutorials",
     "apps.images",
     "apps.books",
-    "apps.classifier",
+    # "apps.classifier",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + REST_FRAMEWORK_APPS + THIRD_PARTY_APPS + LOCAL_APPS
