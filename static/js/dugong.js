4a162be9f5d0d4b10ac1574e8510a2fd8ce2fe3c
jiaxin <edison7500@gmail.com>
2019-07-25 10:42:40 +0800

    update

--- static/js/dugong.js
+++ static/js/dugong.js
@@ -3,84 +3,84 @@
  */
 
 (function ($, document, window) {
-    var utils =  {
-        handlePageScroll: function(){
-            function handleTopLink() {
-                var $topLink = $('#btn-gotop');
-                if($topLink.length){
-                    // console.log($topLink);
-                    if ($(this).scrollTop() > 100) {
-                        $topLink.fadeIn();
-                    } else {
-                        $topLink.fadeOut();
-                    }
-                }
-            }
-            $(window).scroll(handleTopLink)
-        },
+	var utils =  {
+		handlePageScroll: function(){
+			function handleTopLink() {
+				var $topLink = $("#btn-gotop");
+				if($topLink.length){
+					// console.log($topLink);
+					if ($(this).scrollTop() > 100) {
+						$topLink.fadeIn();
+					} else {
+						$topLink.fadeOut();
+					}
+				}
+			}
+			$(window).scroll(handleTopLink);
+		},
 
-        gotop: function() {
-            $("#btn-gotop >a").on('click', function() {
-                $("html, body").animate(
-                    {scrollTop : 0}, 400
-                );
-                return false;
-            });
-        },
+		gotop: function() {
+			$("#btn-gotop >a").on("click", function() {
+				$("html, body").animate(
+					{scrollTop : 0}, 400
+				);
+				return false;
+			});
+		},
 
 
-        showWeixin: function(){
-            var $weixinBtn = $("#btn-wechat >a");
-            $weixinBtn.find('.fa-weixin').mouseover(function(){
-                $weixinBtn.find('div').fadeIn();
-            });
-            $weixinBtn.find('.fa-weixin').mouseout(function(){
-                $weixinBtn.find('div').fadeOut();
-            });
-        },
-        //
-        // refreshCaptcha: function () {
-        //     $('img.captcha').on('click', function(){
-        //         // console.log("OKOKOKO");
-        //         // console.log('click');
-        //         var captcha_img = $(this);
-        //         $.getJSON('/accounts/captcha/', function (res) {
-        //             // console.log(captcha_img);
-        //             captcha_img.attr('src', res['image_url']);
-        //             $('#id_register-captcha_0').val(res['key']);
-        //         });
-        //     });
-        // }
-    };
+		showWeixin: function(){
+			var $weixinBtn = $("#btn-wechat >a");
+			$weixinBtn.find(".fa-weixin").mouseover(function(){
+				$weixinBtn.find("div").fadeIn();
+			});
+			$weixinBtn.find(".fa-weixin").mouseout(function(){
+				$weixinBtn.find("div").fadeOut();
+			});
+		},
+		//
+		// refreshCaptcha: function () {
+		//     $('img.captcha').on('click', function(){
+		//         // console.log("OKOKOKO");
+		//         // console.log('click');
+		//         var captcha_img = $(this);
+		//         $.getJSON('/accounts/captcha/', function (res) {
+		//             // console.log(captcha_img);
+		//             captcha_img.attr('src', res['image_url']);
+		//             $('#id_register-captcha_0').val(res['key']);
+		//         });
+		//     });
+		// }
+	};
 
-    (function init() {
-        utils.handlePageScroll();
-        utils.gotop();
-        // utils.autocomplete();
-        utils.showWeixin();
-        $("#editor").markdown({
-            autofocus: true,
-            language: 'zh',
-        });
-        // utils.refreshCaptcha();
-        $("#autocomplete").autocomplete({
-            minLength:1,
-            scroll: true,
-            max:5,
-            deferRequestBy: 5,
-            noCache: true,
-            source: function(request, response){
-                $.ajax({
-                    url: '/search/autocomplete',
-                    dataType: 'json',
-                    data: request,
-                    success: function(data) {
-                        response( data.results );
-                    }
-                })
-            }
-        });
-        $("fieldset >div.form-group >input").addClass("form-control");
-    })();
+	(function init() {
+		utils.handlePageScroll();
+		utils.gotop();
+		// utils.autocomplete();
+		utils.showWeixin();
+		$("#editor").markdown({
+			autofocus: true,
+			language: "zh",
+		});
+		// utils.refreshCaptcha();
+		$("#autocomplete").autocomplete({
+			minLength:1,
+			scroll: true,
+			max:5,
+			deferRequestBy: 5,
+			noCache: true,
+			source: function(request, response){
+				$.ajax({
+					url: "/search/autocomplete",
+					dataType: "json",
+					data: request,
+					success: function(data) {
+						response( data.results );
+					}
+				});
+			}
+		});
+		$("fieldset >div.form-group >input").addClass("form-control");
+	})();
 
 }(jQuery, document, window));
