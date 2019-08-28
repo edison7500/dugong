45fbaa075329fe13b34a93673223bb7f047a4206
jiaxin <edison7500@gmail.com>
2019-08-28 17:44:11 +0800

    update post sort by created_at desc

--- apps/main/views/__init__.py
+++ apps/main/views/__init__.py
@@ -4,3 +4,7 @@ from haystack.generic_views import SearchView
 class IndexSearchView(SearchView):
     template_name = "search/index.html"
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.order_by("-created_at")
+
