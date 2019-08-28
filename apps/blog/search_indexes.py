3cb49842b8f33e3bbddd9d1131f86b47de9cde62
jiaxin <edison7500@gmail.com>
2019-08-28 13:11:01 +0800

    add search index

---
+++ apps/blog/search_indexes.py
@@ -0,0 +1,13 @@
+from haystack import indexes
+from .models import Post
+
+
+class PostIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr="title")
+
+    def get_model(self):
+        return Post
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.published()
