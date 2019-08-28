5e8ea093bd9d1373e351b21bf25f5daf0e4cc65d
jiaxin <edison7500@gmail.com>
2019-08-28 14:54:25 +0800

    update post index

--- apps/blog/search_indexes.py
+++ apps/blog/search_indexes.py
@@ -6,8 +6,15 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr="title")
 
+    created_at = indexes.DateTimeField(model_attr="craeted_at")
+    updated_at = indexes.DateTimeField(model_attr="updated_at")
+
     def get_model(self):
         return Post
 
     def index_queryset(self, using=None):
         return self.get_model().objects.published()
+
+    def get_updated_field(self):
+        return "updated_at"
+
