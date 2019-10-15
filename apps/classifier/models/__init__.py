6d03de515a858dad67ef3aafdae3f8d9827bc532
jiaxin <edison7500@gmail.com>
2019-10-15 23:29:23 +0800

    update

--- apps/classifier/models/__init__.py
+++ apps/classifier/models/__init__.py
@@ -1,5 +1,5 @@
-from .db import TrainingSet
-
-__all__ = [
-    "TrainingSet"
-]
+# from .db import TrainingSet
+#
+# __all__ = [
+#     "TrainingSet"
+# ]
--- apps/classifier/models/db.py
+++ apps/classifier/models/db.py
@@ -1,10 +1,10 @@
-from django.db import models
-
-
-class TrainingSet(models.Model):
-    body = models.TextField()
-    target = models.CharField(max_length=255)
-    classifier = models.CharField(max_length=100, null=True, blank=True)
-
-    def __str__(self):
-        return "{}: {}".format(self.target, self.body[:20])
+# from django.db import models
+#
+#
+# class TrainingSet(models.Model):
+#     body = models.TextField()
+#     target = models.CharField(max_length=255)
+#     classifier = models.CharField(max_length=100, null=True, blank=True)
+#
+#     def __str__(self):
+#         return "{}: {}".format(self.target, self.body[:20])
