3cf5a1462bdcaf8ae2e73d5b0c86f21c20a30908
jiaxin <edison7500@gmail.com>
2019-12-17 23:42:30 +0800

    update

---
+++ apps/images/tests/test_api.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APITestCase
+
+
+class ImageHandleAPITestCase(APITestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_remove_image_rim(self):
+        pass
