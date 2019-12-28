08a484ebc9373924a054b6ac304d35c4dac569c7
jiaxin <edison7500@gmail.com>
2019-12-28 14:34:26 +0800

    update

--- apps/blog/tests/test_api.py
+++ apps/blog/tests/test_api.py
@@ -1,15 +1,15 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from .post_facker import PostFaker
 
 
 class PostAPITestCase(APITestCase):
 
     def setUp(self) -> None:
-        pass
+        self.post = PostFaker()
 
     def test_post_list_api(self):
-
         url = reverse("api:blog:index")
         req = self.client.get(url)
-        self.assertEqual(req.status_code, status)
+        self.assertEqual(req.status_code, status.HTTP_200_OK)
