451fffc79e183ce6e8dd4a03c8f483ad28be29a3
jiaxin <edison7500@gmail.com>
2020-01-03 23:58:30 +0800

    remove django.contrib.postgres

--- dugong/settings/base.py
+++ dugong/settings/base.py
@@ -42,7 +42,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.flatpages",
     "django.contrib.sitemaps",
-    "django.contrib.postgres",
+    # "django.contrib.postgres",
 ]
 REST_FRAMEWORK_APPS = [
     "rest_framework",
