97e192b20b2f4d69f54c6297628b5bab9f806cd9
jiaxin <edison7500@gmail.com>
2020-02-16 23:10:10 +0800

    update

--- apps/exchangerates/views/__init__.py
+++ apps/exchangerates/views/__init__.py
@@ -7,22 +7,20 @@ logger = logging.getLogger("django")
 
 
 class ExChangeRateListView(generic.ListView):
-    template_name = "exchangerates/list.html"
     model = ExChangeRate
     queryset = ExChangeRate.objects.all()
     paginate_by = 14
 
-    # def res
     def render_to_response(self, context, **response_kwargs):
-        # logger.info(context)
-        date_list = list(map(lambda x: x.date, context["object_list"]))
+        date_list = list(map(lambda x: x.date.strftime("%m-%d"), context["object_list"]))
         date_list.reverse()
-        line_chart = pygal.Line()
-        line_chart.title = "ExChangeRate"
-        line_chart.x_labels = date_list
         cny = list(
             map(lambda y: y.exchange_rates().get("CNY", None), context["object_list"])
         )
         cny.reverse()
+
+        line_chart = pygal.Line()
+        line_chart.title = "ExChangeRate"
+        line_chart.x_labels = date_list
         line_chart.add("CNY", cny)
-        return line_chart.render_django_response()
+        return line_chart.render_django_response(**response_kwargs)
