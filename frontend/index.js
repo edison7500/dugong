4c27d9c0a53a0a40bb1e69c97f487e17a10fb209
jiaxin <edison7500@gmail.com>
2020-03-08 23:19:01 +0800

    update package

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,13 @@
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
-
+import StickySidebar from "sticky-sidebar";
 require('./scss/dugong.scss');
+
+new StickySidebar('#sidebar', {
+  topSpacing: 50,
+  bottomSpacing: 50,
+  additionalMarginTop: 30,
+  resizeSensor: false,
+  // minWidth: 300,
+  containerSelector: '#main-content',
+  innerWrapperSelector: '.sidebar__inner',
+  stickyClass: 'is-affixed',
+});
