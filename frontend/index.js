8a193a2a0d3d94234ff4fb1924375495eb0bbff0
jiaxin <edison7500@gmail.com>
2020-04-12 10:22:53 +0800

    update

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,6 @@ require("./scss/dugong.scss");
 
 import {ImageLazyLoad} from "./src/utils/lazyload";
 import {SSbar} from "./src/utils/sticky_sidebar";
-import "@fancyapps/fancybox"
 
 
 new ImageLazyLoad().run();
