f0d23fe0fa5843c192aa66372cd35c455a20ce2a
jiaxin <edison7500@gmail.com>
2020-04-12 11:45:38 +0800

    update

--- frontend/src/utils/colorbox.js
+++ frontend/src/utils/colorbox.js
@@ -4,8 +4,10 @@ import "jquery-colorbox"
 class Colorbox {
   constructor() {
     let colorbox = $("a.card").colorbox({
+      transition: "fade",
+      fixed: true,
       rel:"gal",
-      close: "<i class='fas fa-times'></i>"
+      close: "<i class='fas fa-times'></i>",
     });
   }
 }
