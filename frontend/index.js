b8120dd3e1bcba7d3c37dcb38a01f9680ee046c2
jiaxin <edison7500@gmail.com>
2020-04-12 09:06:40 +0800

    fixed ci

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,8 @@
 require("./scss/dugong.scss");
 
-// import StickySidebar from "sticky-sidebar";
 import {ImageLazyLoad} from "./src/utils/lazyload";
 import {SSbar} from "./src/utils/sticky_sidebar";
 
 
 new ImageLazyLoad().run();
 new SSbar();
-
-// let sidebar = document.getElementById("sidebar");
-//
-// if (sidebar) {
-//   new StickySidebar(sidebar, {
-//     topSpacing: 20,
-//     bottomSpacing: 20,
-//     // additionalMarginTop: 30,
-//     resizeSensor: true,
-//     // minWidth: 300,
-//     containerSelector: "#main-content",
-//     // containerSelector: false,
-//     innerWrapperSelector: ".sidebar__inner",
-//     stickyClass: "is-affixed",
-//   });
-// }
-
-
