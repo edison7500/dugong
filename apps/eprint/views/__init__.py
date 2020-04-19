3c4e3934524c840c71006163635fd4e0d1b1fd87
jiaxin <edison7500@gmail.com>
2020-04-19 21:18:58 +0800

    add testcase

--- apps/eprint/views/__init__.py
+++ apps/eprint/views/__init__.py
@@ -8,6 +8,24 @@ class EprintListView(generic.ListView):
     paginate_by = 30
     template_name = "eprint/list.html"
 
+    @property
+    def category(self):
+        return self.request.GET.get("category", None)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.category:
+            return qs.filter(category=self.category)
+        else:
+            return qs
+
+    def get_context_data(self, **kwargs):
+        _context = super().get_context_data(**kwargs)
+        _context.update({
+            "category": self.category,
+        })
+        return _context
+
 
 class EprintDetailView(generic.DetailView):
     model = Eprint
