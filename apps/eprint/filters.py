b8caecd35d3d47a4c31baf0b7cec683c707dd3d5
jiaxin <edison7500@gmail.com>
2020-04-25 16:38:27 +0800

    update

--- apps/eprint/filters.py
+++ apps/eprint/filters.py
@@ -7,7 +7,7 @@ class EprintFilterSet(django_filters.FilterSet):
     author = django_filters.CharFilter(field_name="authors", method="filter_author")
 
     def filter_author(self, queryset, name, value):
-        lookup = '__'.join([name, 'contains'])
+        lookup = "__".join([name, "contains"])
         return queryset.filter(**{lookup: [value]})
 
     class Meta:
