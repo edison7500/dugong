addef7004204429114db84e2a10a84a25e82a378
jiaxin <edison7500@gmail.com>
2020-04-25 16:35:34 +0800

    update

--- apps/eprint/filters.py
+++ apps/eprint/filters.py
@@ -4,7 +4,12 @@ from .models import Eprint
 
 class EprintFilterSet(django_filters.FilterSet):
     category = django_filters.CharFilter(lookup_expr="iexact")
+    author = django_filters.CharFilter(field_name="authors", method="filter_author")
+
+    def filter_author(self, queryset, name, value):
+        lookup = '__'.join([name, 'contains'])
+        return queryset.filter(**{lookup: [value]})
 
     class Meta:
         model = Eprint
-        fields = ["category"]
+        fields = ["category", "author"]
