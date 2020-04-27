828fa0ca77fd661e4b1a85eca5a429780636654f
jiaxin <edison7500@gmail.com>
2020-04-27 17:03:15 +0800

    add filter keywords

--- apps/eprint/filters.py
+++ apps/eprint/filters.py
@@ -5,10 +5,15 @@ from .models import Eprint
 class EprintFilterSet(django_filters.FilterSet):
     category = django_filters.CharFilter(lookup_expr="iexact")
     author = django_filters.CharFilter(field_name="authors", method="filter_author")
+    keyword = django_filters.CharFilter(field_name="keywords", method="filter_keyword")
 
     def filter_author(self, queryset, name, value):
         lookup = "__".join([name, "contains"])
         return queryset.filter(**{lookup: [value]})
+
+    def filter_keyword(self, queyset, name, value):
+        lookup = "__".join([name, "contains"])
+        return queyset.filter(**{lookup: [value]})
 
     class Meta:
         model = Eprint
