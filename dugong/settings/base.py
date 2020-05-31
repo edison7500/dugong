fdd6525a41a59c470d33b16d2f35cc27e25c78f0
jiaxin <edison7500@gmail.com>
2020-05-31 21:22:03 +0800

    update

--- dugong/settings/base.py
+++ dugong/settings/base.py
@@ -204,15 +204,15 @@ TAGGIT_CASE_INSENSITIVE = True
 # django cache
 # ----------------------------------------------------------------------------------------------------------------------
 #
-CACHES = {
-    "default": {
-        "BACKEND": "diskcache.DjangoCache",
-        "LOCATION": env("CACHE_PATH", default="/tmp/cache"),
-        "SHARDS": 4,
-        "DATABASE_TIMEOUT": 1.0,
-        "OPTIONS": {"size_limit": 2 ** 32},  # 4 gigabytes
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "diskcache.DjangoCache",
+#         "LOCATION": env("CACHE_PATH", default="/tmp/cache"),
+#         "SHARDS": 4,
+#         "DATABASE_TIMEOUT": 1.0,
+#         "OPTIONS": {"size_limit": 2 ** 32},  # 4 gigabytes
+#     }
+# }
 
 # csrf view
 # ----------------------------------------------------------------------------------------------------------------------
