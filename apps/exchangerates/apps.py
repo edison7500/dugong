0790a05b430665392a7548d88fea3237d1a6aff6
jiaxin <edison7500@gmail.com>
2020-06-07 21:00:44 +0800

    update

--- apps/exchangerates/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExchangeratesConfig(AppConfig):
-    name = "exchangerates"
--- apps/exchangerates/urls/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.exchangerates.views import ExChangeRateListView
-
-app_name = "exchangerates"
-
-urlpatterns = [path("", ExChangeRateListView.as_view(), name="index")]
