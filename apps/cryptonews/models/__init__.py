729b1550df58e81f93e6fe2fa16e21a335af7c6d
papaya <edison7500@gmail.com>
2021-11-30 21:02:34 +0800

    update

---
+++ apps/cryptonews/models/__init__.py
@@ -0,0 +1,5 @@
+from .db import News
+
+__all__ = [
+    "News",
+]
---
+++ apps/cryptonews/models/db.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+
+class News(models.Model):
+    title = models.CharField(max_length=1024)
+    content = models.TextField(blank=True, default="")
+    published_at = models.DateTimeField(default=timezone.now, db_index=True)
+    link = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
