ff4f6c46d97d4c6eba4a8df89305b8afdfa86d3f
papaya <edison7500@gmail.com>
2021-12-01 00:04:22 +0800

    update

--- apps/cryptonews/models/db.py
+++ apps/cryptonews/models/db.py
@@ -14,7 +14,12 @@ class News(models.Model):
     title = models.CharField(max_length=1024)
     content = models.TextField(blank=True, default="")
     published_at = models.DateTimeField(default=timezone.now, db_index=True)
-    origin_link = models.URLField(max_length=1024, null=True, blank=True)
+    origin_link = models.URLField(
+        max_length=1024, null=True, blank=True, unique=True
+    )
+
+    class Meta:
+        ordering = ["-published_at"]
 
     def __str__(self):
         return f"{self.title} - {self.published_at} - {self.origin_link}"
