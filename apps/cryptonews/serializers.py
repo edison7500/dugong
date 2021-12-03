dd0aba749751de268d7776379f3382ebbd0ad053
papaya <edison7500@gmail.com>
2021-12-03 15:56:17 +0800

    update

--- apps/cryptonews/serializers.py
+++ apps/cryptonews/serializers.py
@@ -4,8 +4,22 @@ from .models import News
 
 class CryptoNewsSerializer(serializers.ModelSerializer):
     # domain = serializers.CharField()
-    channel = serializers.CharField(default="jiaxindevchannels")
 
     class Meta:
         model = News
-        exclude = ["id", "slug", "content"]
+        exclude = ["id"]
+
+
+class PushExchangeAnnSerializer(serializers.ModelSerializer):
+
+    channel = serializers.CharField(
+        default="jiaxindevchannels", read_only=True
+    )
+
+    class Meta:
+        model = News
+        exclude = [
+            "id",
+            "content",
+            "slug",
+        ]
--- apps/cryptonews/signals.py
+++ apps/cryptonews/signals.py
@@ -1,13 +1,13 @@
 import requests
 import logging
 from apps.cryptonews.models import News
-from apps.cryptonews.serializers import CryptoNewsSerializer
+from apps.cryptonews.serializers import PushExchangeAnnSerializer
 
 logger = logging.getLogger("django")
 
 
 def push_crypto_new(sender, instance: News, created, **kwargs):
     if isinstance(instance, sender) and created:
-        ser = CryptoNewsSerializer(instance=instance)
+        ser = PushExchangeAnnSerializer(instance=instance)
         # logger.info(ser.data)
         requests.post(url="http://tg-bot:5000/push", json=ser.data)
