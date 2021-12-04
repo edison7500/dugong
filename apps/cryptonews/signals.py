9735266442406cd3375a658beb1b5eb73ae76275
papaya <edison7500@gmail.com>
2021-12-04 20:48:35 +0800

    update

--- apps/cryptonews/signals.py
+++ apps/cryptonews/signals.py
@@ -1,13 +1,19 @@
 import requests
 import logging
+from django.conf import settings
 from apps.cryptonews.models import News
 from apps.cryptonews.serializers import PushExchangeAnnSerializer
 
 logger = logging.getLogger("django")
+
+debug = getattr(settings, "DEBUG")
 
 
 def push_crypto_new(sender, instance: News, created, **kwargs):
     if isinstance(instance, sender) and created:
         ser = PushExchangeAnnSerializer(instance=instance)
         # logger.info(ser.data)
-        requests.post(url="http://tg-bot:5000/push", json=ser.data)
+        if debug:
+            logger.info(ser.data)
+        # else:
+        #     requests.post(url="http://tg-bot:5000/push", json=ser.data)
