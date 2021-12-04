40b25d7d3232dd7b44ed6d395ef69b70da4e40e2
papaya <edison7500@gmail.com>
2021-12-04 21:11:14 +0800

    update

--- apps/cryptonews/signals.py
+++ apps/cryptonews/signals.py
@@ -12,8 +12,7 @@ debug = getattr(settings, "DEBUG")
 def push_crypto_new(sender, instance: News, created, **kwargs):
     if isinstance(instance, sender) and created:
         ser = PushExchangeAnnSerializer(instance=instance)
-        # logger.info(ser.data)
         if debug:
             logger.info(ser.data)
-        # else:
-        #     requests.post(url="http://tg-bot:5000/push", json=ser.data)
+        else:
+            requests.post(url="http://tg-bot:5000/push", json=ser.data)
