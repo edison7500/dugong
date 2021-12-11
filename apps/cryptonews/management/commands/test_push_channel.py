f1835e2e72a208aa969d39fd954984c0a15a86de
papaya <edison7500@gmail.com>
2021-12-11 20:50:33 +0800

    update

--- apps/cryptonews/management/commands/test_push_channel.py
+++ apps/cryptonews/management/commands/test_push_channel.py
@@ -1,6 +1,8 @@
-import json
+# import json
+import re
 import requests
-from datetime import datetime, timedelta
+
+# from datetime import datetime, timedelta
 from django.core.management import BaseCommand
 from django.conf import settings
 from apps.cryptonews.models import News
@@ -9,35 +11,37 @@ from apps.cryptonews.serializers import PushExchangeAnnSerializer
 translate_url = getattr(settings, "TRANSLATE_BOT_URL")
 
 
-def translate_text(text, source="ok", target="en") -> str:
-    _r = requests.post(
-        translate_url,
-        json={
-            "text": text,
-            "source": source,
-            "target": target,
-        },
-    )
-    _data = json.loads(_r.text)
-    return _data["TranslatedText"]
+# def translate_text(text, source="ok", target="en") -> str:
+#     _r = requests.post(
+#         translate_url,
+#         json={
+#             "text": text,
+#             "source": source,
+#             "target": target,
+#         },
+#     )
+#     _data = json.loads(_r.text)
+#     return _data["TranslatedText"]
+
+p = re.compile(r"^\[.*?\]")
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        _expire = datetime.utcnow() - timedelta(days=1)  # noqa
+        # _expire = datetime.utcnow() - timedelta(days=1)  # noqa
         news = News.objects.filter(origin_link__icontains="upbit").first()
         # print(_expire.astimezone())
         # if _expire.timestamp() < news.published_at.timestamp():
         ser = PushExchangeAnnSerializer(instance=news)
         _data = ser.data
 
-        if (
-            news.domain in ["upbit.com", "cafe.bithumb.com"]
-            and translate_url is not None
-        ):
-            _title = _data["title"]
-            _title = translate_text(_title, "ko", "en")
-            _data.update({"title": _title})
+        # if (
+        #     news.domain in ["upbit.com", "cafe.bithumb.com"]
+        #     and translate_url is not None
+        # ):
+        _title = _data["title"]
+        _title = p.sub("", _title).strip()
+        _data.update({"title": f"[upbit] {_title}"})
 
-        # _data.update({"channel": "testAnnChannel"})
+        _data.update({"channel": "testAnnChannel"})
         requests.post(url="http://tg-bot:5000/push", json=_data)
