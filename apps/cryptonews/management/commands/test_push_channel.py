677774f735bba7be6f9338a2e379647dc4c2724a
papaya <edison7500@gmail.com>
2021-12-11 13:59:46 +0800

    update

---
+++ apps/cryptonews/management/commands/test_push_channel.py
@@ -0,0 +1,14 @@
+import requests
+from django.core.management import BaseCommand
+from apps.cryptonews.models import News
+from apps.cryptonews.serializers import PushExchangeAnnSerializer
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        news = News.objects.first()
+        ser = PushExchangeAnnSerializer(instance=news)
+        _data = ser.data
+        _data.update("channel", "testAnnChannel")
+        print(_data)
+        # requests.post(url="http://tg-bot:5000/push", json=ser.data)
