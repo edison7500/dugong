cfd66bd7955bfb1590ab61d4b3a154ed65b81ef1
papaya <edison7500@gmail.com>
2021-12-11 14:22:01 +0800

    update

--- apps/cryptonews/management/commands/test_push_channel.py
+++ apps/cryptonews/management/commands/test_push_channel.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timedelta
 from django.core.management import BaseCommand
 from apps.cryptonews.models import News
 from apps.cryptonews.serializers import PushExchangeAnnSerializer
@@ -6,9 +7,12 @@ from apps.cryptonews.serializers import PushExchangeAnnSerializer
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        _expire = datetime.utcnow() - timedelta(days=1)  # noqa
         news = News.objects.first()
+        # print(_expire.astimezone())
+        # if _expire.timestamp() < news.published_at.timestamp():
         ser = PushExchangeAnnSerializer(instance=news)
         _data = ser.data
-        _data.update({"channel", "testAnnChannel"})
+        _data.update({"channel": "testAnnChannel"})
         print(_data)
-        # requests.post(url="http://tg-bot:5000/push", json=ser.data)
+        requests.post(url="http://tg-bot:5000/push", json=ser.data)
