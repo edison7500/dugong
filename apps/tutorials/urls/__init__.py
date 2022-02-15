7cdedda38b7bc0eead29d77d94126ec1b2e4bba7
papaya <edison7500@gmail.com>
2022-02-15 21:28:42 +0800

    update

--- apps/tutorials/urls/__init__.py
+++ apps/tutorials/urls/__init__.py
@@ -1,11 +1,10 @@
-# from django.conf.urls import url
-from django.urls import path
-
-from apps.tutorials.views.web import TutorialListView, TutorialDetailView
-
-app_name = "tutorials"
-
-urlpatterns = [
-    path("", TutorialListView.as_view(), name="list"),
-    path("<slug:slug>.htm", TutorialDetailView.as_view(), name="detail"),
-]
+# from django.urls import path
+#
+# from apps.tutorials.views.web import TutorialListView, TutorialDetailView
+#
+# app_name = "tutorials"
+#
+# urlpatterns = [
+#     path("", TutorialListView.as_view(), name="list"),
+#     path("<slug:slug>.htm", TutorialDetailView.as_view(), name="detail"),
+# ]
