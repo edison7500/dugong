0ee9cb47cca9a660efb6d5b2f0c2f4b2b5fe5ee9
papaya <edison7500@gmail.com>
2022-09-24 17:45:11 +0800

    update

--- apps/toolbox/models/db.py
+++ apps/toolbox/models/db.py
@@ -49,7 +49,7 @@ class ToolBox(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
     url = models.URLField(max_length=255)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=False)
 
     tags = TaggableManager(blank=True)
 
